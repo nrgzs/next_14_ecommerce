9b66c1808ab1f0b0d576c9ae0830d5ad28800bf8
nergız seyidova <nergisseyidova@gmail.com>
2024-08-20 13:05:37 +0400

    personal test usage

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,12 @@ import Specials from '@/components/Specials/Specials';
 
 export default function Home() {
   return (
-    <main >
+    <main>
+        <iframe
+          src="https://player.vimeo.com/video/999027855?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+          title="Set up your first video"
+        ></iframe>
       <Hero />
       <Features />
       <Specials />
